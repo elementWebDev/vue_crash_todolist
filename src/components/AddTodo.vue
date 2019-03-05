c14e625100c8fa89d21cd2000c464c2db08573ec
elementWebDev <ivinci.design@gmail.com>
2019-03-04 18:51:04 -0800

    Revert "Update AddTodo.vue"
    This reverts commit 172c73c618782ef2fb0960ecd7c9d6ea04373915.

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<form @submit.prevent="addTodo">
+		<form @submit="addTodo">
 			<input type="text" v-model="title" name="title" placeholder="Add Todo...">
 			<input type="submit" value="Submit" class="btn">
 		</form>
@@ -19,7 +19,8 @@ export default {
 		}
 	},
 	methods: {
-		addTodo() {
+		addTodo(e) {
+			e.preventDefault();
 			const newTodo = {
 				//id: uuid.v4(),
 				title: this.title,
